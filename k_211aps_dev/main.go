3f3e57c6edb5ca99e2b9464e3b8826758e48a0f5
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-08-26 06:53:54 +0900

    Fixed codes.

--- k_211aps_dev/main.go
+++ k_211aps_dev/main.go
@@ -15,7 +15,7 @@ type EMPLOYEE struct {
 func main() {
 //  db, err := sql.Open("postgres", "host=db port=5432 user=root sslmode=disable")                  20220825修正
 //  db, err := sql.Open("postgres", "host=311dbs_dev port=5432 user=postgres sslmode=disable")
-    db, err := sql.Open("hoge", "host=311dbs_dev port=5432 user=hoge sslmode=disable")           // 20220826修正／postgres->hoge
+    db, err := sql.Open("postgres", "host=311dbs_dev port=5432 user=hoge sslmode=disable")           // 20220826修正／postgres->hoge
 
     defer db.Close()
 
