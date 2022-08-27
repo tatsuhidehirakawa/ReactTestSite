e8dc0990c1eac9b5ec6032972b46c2b8c4ab8345
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-08-27 14:57:00 +0900

    Fixed codes.

--- k_111wbs_dev/src/App.jsx
+++ k_111wbs_dev/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import Main from "./Main";
-import Footer from "./Footer";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 import "./App.scss";
 
