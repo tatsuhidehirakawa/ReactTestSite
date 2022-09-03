24742de518bbcae7049581a1b55115616add1458
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-03 15:35:22 +0900

    Fixed code

--- k_901set_dev/311set_dev/initdb/03_initdata.sql
+++ k_901set_dev/311set_dev/initdb/03_initdata.sql
@@ -1,8 +1,7 @@
 --初期データ(管理者用データ)の記述
+
 INSERT INTO account_master
   (user_id,account_name,password,time_zone,language)
 VALUES
   ('5e9nr6eka93u','Admin','${PGSQL_PW}','Asia/Tokyo');
 
-
---初期データ(管理者ユーザのログイン情報)をマルチプルインサートを利用し記述する
