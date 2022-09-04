fab91d86c2cd33f370b3ad13f2e8197fd48d3ba2
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-04 23:30:27 +0900

    Change directory structure

--- k_901set_dev/310set_dev/initdb/01_initialize.sql
+++ k_901set_dev/310set_dev/initdb/01_initialize.sql
@@ -1,15 +1,14 @@
 -- DB作成
---CREATE DATABASE hogedb; 
+CREATE DATABASE hogedb; 
 
 -- 作成したDBへ切り替え
---\c hogedb
+\c hogedb
 
 -- スキーマ作成
---CREATE SCHEMA hogeschema;
+CREATE SCHEMA hogeschema;
 
 -- ロールの作成
 CREATE ROLE ${PGSQL_ID} WITH LOGIN PASSWORD '${PGSQL_PW}';
 
 -- 権限追加
---GRANT ALL PRIVILEGES ON SCHEMA hogeschema TO hoge;
-GRANT ALL PRIVILEGES ON *.* TO hoge;
+GRANT ALL PRIVILEGES ON SCHEMA hogeschema TO hoge;
--- k_901set_dev/311set_dev/initdb/03_initdata.sql
+++
@@ -1,7 +0,0 @@
---初期データ(管理者用データ)の記述
-
-INSERT INTO account_master
-  (user_id,account_name,password,time_zone,language)
-VALUES
-  ('5e9nr6eka93u','Admin','${PGSQL_PW}','Asia/Tokyo');
-
