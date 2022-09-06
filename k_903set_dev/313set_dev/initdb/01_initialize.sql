68c77bf348048e159444a813c88b10d4b971e211
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-07 00:01:09 +0900

    Add comments

--- k_903set_dev/313set_dev/initdb/01_initialize.sql
+++ k_903set_dev/313set_dev/initdb/01_initialize.sql
@@ -8,11 +8,18 @@
 --CREATE SCHEMA hogeschema;
 
 -- ロールの作成
-
 CREATE ROLE hoge WITH LOGIN PASSWORD 'passw0rd';
--- CREATE ROLE '${PGSQL_ID}' WITH LOGIN PASSWORD '${PGSQL_PW}';
--- ↑.sqlで環境変数は使えない！？
 
 -- 権限追加
 --GRANT ALL PRIVILEGES ON SCHEMA hogeschema TO hoge;
 GRANT ALL PRIVILEGES ON *.* TO hoge;
+
+
+
+-- 「.sql」では環境変数は使えないので注意！
+
+-- 「01_initialize.sql」は主としてDDL、DCLを記述する。
+
+-- DBの管理ユーザ以外にDBを操作できる権限の弱いユーザを
+-- 作成する場合のみ「01_initialize.sql」が必要となる。
+-- DBの管理ユーザは.ymlで環境変数を利用して記述する。
