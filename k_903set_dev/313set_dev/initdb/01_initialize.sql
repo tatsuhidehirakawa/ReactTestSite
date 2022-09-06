6da46aa082ec1d7768e632738ac90d80c41590ad
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-06 13:30:11 +0900

    .SQLから環境変数を除去

--- k_903set_dev/313set_dev/initdb/01_initialize.sql
+++ k_903set_dev/313set_dev/initdb/01_initialize.sql
@@ -8,7 +8,10 @@
 --CREATE SCHEMA hogeschema;
 
 -- ロールの作成
-CREATE ROLE '${PGSQL_ID}' WITH LOGIN PASSWORD '${PGSQL_PW}';
+
+CREATE ROLE hoge WITH LOGIN PASSWORD 'passw0rd';
+-- CREATE ROLE '${PGSQL_ID}' WITH LOGIN PASSWORD '${PGSQL_PW}';
+-- ↑.sqlで環境変数は使えない！？
 
 -- 権限追加
 --GRANT ALL PRIVILEGES ON SCHEMA hogeschema TO hoge;
