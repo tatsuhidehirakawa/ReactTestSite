5e68d07bef126b2837a9fc136aa972bf33fa6b9e
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-11 19:37:42 +0900

    Minot code fixes.

--- k_213api_dev/go/web/main.go
+++ k_213api_dev/go/web/main.go
@@ -7,22 +7,12 @@ import (
 func main() {
     router := gin.Default()  // *gin.EngineをReturnする関数を宣言
 
-    router.GET(
-/*---第一引数／エンドポイントを指定---+-----+-----+-----+-----+---*/
-        "/",
-/*---第二引数／コントローラーを指定---+-----+-----+-----+-----+---*/
-        func(
-            c *gin.Context
-            )
-            {  // 
-                c.JSON(  // 
-                    200,  // 
-                    gin.H
-                    {"message": "Hello World",}
-                )
-            }
-/*---+-----+-----+-----+------+-----+-----+-----+-----+-----+---*/
-    )
+    router.GET("/", func(c *gin.Context) {
+        c.JSON(200, gin.H{
+            "message": "Hello World",
+        })
+    })
+    
     router.Run(":3000")  // サーバを起動し3000番で待ち受け
 }
 
