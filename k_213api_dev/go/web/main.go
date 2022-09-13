e3ba32bb8e96dbc87d5734a3f898efe998e6d073
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-13 16:18:51 +0900

    Update comments

--- k_213api_dev/go/web/main.go
+++ k_213api_dev/go/web/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 
-    // *gin.EngineをReturnする関数を宣言
+    // *gin.EngineをReturnする関数を宣言…<1>
     router := gin.Default()
 
     // jsonデータをGETさせる処理
@@ -16,22 +16,22 @@ func main() {
         })
     })
 
-    // GO構造体をjsonに変換する処理
+    // GO構造体をjsonに変換する処理…<2>
 
 
 
 
-    // 取り出した値をGO構造体に格納する処理
+    // 取り出した値をGO構造体に格納する処理…<3>
 
 
 
 
-    // 値を取り出すSQL文をDBに投げる処理(GORM？)
+    // 値を取り出すSQL文をDBに投げる処理(GORM？)…<4>
 
 
 
 
-    // サーバを起動する処理
+    // サーバを起動する処理…<5>
     router.Run(":3000")
 }
 
