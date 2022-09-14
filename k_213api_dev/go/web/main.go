83eccd8d2018a57654d6227c279346e4ba3997cb
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-14 18:43:40 +0900

    Add codes

--- k_213api_dev/go/web/main.go
+++ k_213api_dev/go/web/main.go
@@ -8,6 +8,26 @@ import (
     "net/http"                   // 追加
 )
 
+type txjsn struct {
+    // Table Name:"account_attribute"
+    account_id        int         `json: "account_id"`
+    update_date       timestamp   `json: "update_date"`
+    update_id         string      `json: "update_id"`
+    pic_id_01         string      `json: "pic_id_01"`
+    skil              string      `json: "skil"`
+    // Table Name:"account_master"
+    user_id           string      `json: "user_id"`
+    account_name      string      `json: "account_name"`
+    mail_addless      string      `json: "mail_addless"`
+    password          string      `json: "password"`
+    zip_code          string      `json: "zip_code"`
+    barth_date        timestamp   `json: "barth_date"`
+    time_zone         string      `json: "time_zone"`
+    language          string      `json: "language"`
+    status            string      `json: "status"`
+    registration_date string `json: "registration_date"`
+}
+
 func main() {
 
     // *gin.EngineをReturnする関数を宣言
@@ -18,26 +38,7 @@ func main() {
     router.GET("/", func(c *gin.Context) {
 
     // GO構造体をjsonに変換する処理…<1>
-    type txjsn struct {
-        // Table Name:"account_attribute"
-        account_id        int         `json: "account_id"`
-        update_date       timestamp   `json: "update_date"`
-        update_id         string      `json: "update_id"`
-        pic_id_01         string      `json: "pic_id_01"`
-        skil              string      `json: "skil"`
-        // Table Name:"account_master"
-        user_id           string      `json: "user_id"`
-        account_name      string      `json: "account_name"`
-        mail_addless      string      `json: "mail_addless"`
-        password          string      `json: "password"`
-        zip_code          string      `json: "zip_code"`
-        barth_date        timestamp   `json: "barth_date"`
-        time_zone         string      `json: "time_zone"`
-        language          string      `json: "language"`
-        status            string      `json: "status"`
-        registration_date string `json: "registration_date"`
-    }
-
+ 
     func main() {
         // Table Name:"account_attribute"
         txjsn := new(txjsn)
