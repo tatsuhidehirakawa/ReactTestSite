d8e86a1c45324880971806de736ad346ab7938e3
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-15 18:05:55 +0900

    Updata codes

--- k_213api_dev/go/web/main.go
+++ k_213api_dev/go/web/main.go
@@ -2,33 +2,33 @@ package main
 
 import (
     "github.com/gin-gonic/gin"
+
     "bytes"                      // 追加
     "encoding/json"              // 追加
     "fmt"                        // 追加
     "net/http"                   // 追加
 )
 
-/* models/models.goへ移植
-type txjsn struct {
-    // Table Name:"account_attribute"
-    account_id        int         `json: "account_id"`
-    update_date       timestamp   `json: "update_date"`
-    update_id         string      `json: "update_id"`
-    pic_id_01         string      `json: "pic_id_01"`
-    skil              string      `json: "skil"`
-    // Table Name:"account_master"
-    user_id           string      `json: "user_id"`
-    account_name      string      `json: "account_name"`
-    mail_addless      string      `json: "mail_addless"`
-    password          string      `json: "password"`
-    zip_code          string      `json: "zip_code"`
-    barth_date        timestamp   `json: "barth_date"`
-    time_zone         string      `json: "time_zone"`
-    language          string      `json: "language"`
-    status            string      `json: "status"`
-    registration_date string `json: "registration_date"`
+type Account struct {
+    account_id        int       `json:"account_id"`
+    update_date       time.Time `json:"update_date"`
+    update_id         string    `json:"update_id"`
+    pic_id_01         string    `json:"pic_id_01"`
+    skil              string    `json:"skil"`
 }
-*/
+
+type AccountMaster struct {
+    user_id           string    `json:"user_id"`
+    account_name      string    `json:"account_name"`
+    mail_addless      string    `json:"mail_addless"`
+    password          string    `json:"password"`
+    zip_code          string    `json:"zip_code"`
+    barth_date        time.Time `json:"barth_date"`
+    time_zone         string    `json:"time_zone"`
+    language          string    `json:"language"`
+    status            string    `json:"status"`
+    registration_date string    `json: "registration_date"`
+}
 
 func main() {
 
@@ -41,7 +41,6 @@ func main() {
 
     // GO構造体からjsonに変換する処理…<#001>
  
-    func main() {
         // Table Name:"account_attribute"
         txjsn := new(txjsn)
         txjsn.account_id = "account_id"
@@ -106,44 +105,3 @@ func main() {
             "registration_date": "registration_date",
         })
     })
-/*---POST処理----+-----+-----+-----+-----+-----+-----+-----+---*/
-    // jsonデータをPOSTさせる処理
-    router.POST("/", func(c *gin.Context) {
-
-    // jsonをGO構造体に変換する処理…<#002>
-
-
-
-    // GO構造体から取り出した値をDBに格納する処理…<#004>
-
-
-
-    // 値を取り出すSQL文をDBに投げる処理…<#008>
-
-
-
-    // gin.H{}でHTMLテンプレートからgoファイルの変数を受け取る…<#006>
-    c.JSON(200, gin.H{
-            // Table Name:"account_attribute"
-            "account_id": "account_id",
-            "update_date": "update_date",
-            "update_id": "update_id",
-            "pic_id_01": "pic_id_01",
-            "skil": "skil",
-            // Table Name:"account_master"
-            "user_id": "user_id",
-            "account_name": "account_name",
-            "mail_addless": "mail_addless",
-            "password": "password",
-            "zip_code": "zip_code",
-            "barth_date": "barth_date",
-            "time_zone": "time_zone",
-            "language": "language",
-            "status": "status",
-            "registration_date": "registration_date",
-        })
-    })
-/*---+-----+-----+-----+-----+-----+-----+-----+-----+-----+---*/
-    // サーバを起動する処理
-    router.Run(":3000")
-}
