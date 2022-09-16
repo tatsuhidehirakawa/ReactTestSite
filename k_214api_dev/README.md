e8f8c7e20899644cbf3e232a629c42dec75aa855
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-16 13:47:13 +0900

    Change directory structure.

---
+++ k_214api_dev/README.md
@@ -0,0 +1,15 @@
+APIサーバ構築練習①「k214api_dev」  
+参考記事：  
+コマンド：  
+```
+docker compose up # ビルド
+docker compose exec web bash # コンテナ入系
+go mod init github.com/gin-gonic/gin/v2 # Golangのバージョンに注意
+go get # 追加コマンド
+go run main.go # サーバ起動
+start chrome http://localhost:3000/
+```
+<!--
+cd "C:\Users\tatsu_hira_s\Documents\My Repository\myportfolio_k\k_214api_dev\go"; docker compose up -d; docker compose exec web bash; start chrome http://localhost:3000/
+go mod init github.com/gin-gonic/gin/v2 && go get && go run main.go
+-->
---
+++ k_214api_dev/go/web/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Go構造体「Account_attribute」のデータ型を定義
+type Account_attribute struct {
+	Account_id  int       `json:"account_id"`
+	Update_date time.Time `json:"update_date"`
+	Update_id   string    `json:"update_id"`
+	Pic_id_01   string    `json:"pic_id_01"`
+	Skil        string    `json:"skil"`
+}
+
+func main() {
+
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+
+		// Go構造体「Account_attribute」の初期化
+		account_attribute := new(Account_attribute)
+		account_attribute.Account_id = 1234
+		account_attribute.Update_date = time.Now()
+		account_attribute.Update_id = "update_id"
+		account_attribute.Pic_id_01 = "pic_id_01"
+		account_attribute.Skil = "skil"
+
+		t, _ := json.Marshal(account_attribute) // 「_」は第2戻り値を破棄するブランク変数
+		c.JSON(http.StatusOK, string(t))
+	})
+
+	router.Run(":3000")
+}
