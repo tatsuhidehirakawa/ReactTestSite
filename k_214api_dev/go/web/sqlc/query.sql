efcba0de782068191e5e78e69d9b80988e3cd834
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-17 21:04:51 +0900

    Add codes for sqlc setting.

--- k_214api_dev/go/web/sqlc/query.sql
+++ k_214api_dev/go/web/sqlc/query.sql
@@ -1,21 +1,21 @@
 -- name: GetAccount_master :one
 SELECT * FROM account_master
-WHERE account_id = ? LIMIT 1;
+WHERE user_id = ? LIMIT 1;
 
 -- name: ListAccount_master :many
 SELECT * FROM account_master
-ORDER BY name;
+ORDER BY user_id;
 
 -- name: CreateAccount_master :execresult
 INSERT INTO account_master (
-  update_date, update_id, pic_id_01, skill
+  user_id, account_name, mail_addless, password, zip_code, barth_date, time_zone, language, status, registration_date, rate
 ) VALUES (
   ?, ?
 );
 
 -- name: DeleteAccount_master :exec
 DELETE FROM account_master
-WHERE account_id = ?;
+WHERE user_id = ?;
 
 
 -- name: GetAccount_attribute :one
@@ -24,11 +24,11 @@ WHERE account_id = ? LIMIT 1;
 
 -- name: ListAccount_attribute :many
 SELECT * FROM account_attribute
-ORDER BY name;
+ORDER BY account_id;
 
 -- name: CreateAccount_attribute :execresult
 INSERT INTO account_attribute (
-  update_date, update_id, pic_id_01, skill
+  account_id, self_introduction, twitter_url, facebook_url, instagram_url, youtube_channel, update_date, update_id, pic_id_01, pic_id_02, pic_id_03, skill
 ) VALUES (
   ?, ?
 );
