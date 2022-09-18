4ff87557c098701323ed341fa6dc676da23811de
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-18 10:36:39 +0900

    Add codes for sqlc setting.

--- k_214api_dev/go/web/main.go
+++ k_214api_dev/go/web/main.go
@@ -5,8 +5,8 @@ import (
 	"database/sql"
 	"log" // ?
 	"context" // ?
-	"github.com/lib/pq"
-	"github.com/gowebexamples/sqlc-example/postgres"
+	"github.com/lib/pq" // ?
+	"github.com/gowebexamples/sqlc-example/postgres" // ?
 	"github.com/gin-gonic/gin"
 )
 
