16a558ff2d2a7b6634f046897be0d34f8a56d37a
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-19 22:53:49 +0900

    Fixed codes.

--- k_214api_dev/go/web/go.mod
+++ k_214api_dev/go/web/go.mod
@@ -1,29 +1,30 @@
-module github.com/goark/pa-api
+module github.com/myportfolio_k/k_214api_dev/go/web
 
 go 1.19
 
 require (
-	github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220626175859-9abda183db8e // indirect
-	github.com/bytecodealliance/wasmtime-go v0.39.0 // indirect
-	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
-	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/kyleconroy/sqlc v1.15.0 // indirect
-	github.com/pganalyze/pg_query_go/v2 v2.1.2 // indirect
-	github.com/pingcap/errors v0.11.5-0.20210425183316-da1aaba5fb63 // indirect
-	github.com/pingcap/log v0.0.0-20210906054005-afc726e70354 // indirect
-	github.com/pingcap/tidb/parser v0.0.0-20220725134311-c80026e61f00 // indirect
-	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
-	github.com/spf13/cobra v1.5.0 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
-	go.uber.org/atomic v1.9.0 // indirect
-	go.uber.org/multierr v1.7.0 // indirect
-	go.uber.org/zap v1.19.1 // indirect
-	golang.org/x/exp v0.0.0-20220428152302-39d4317da171 // indirect
+	github.com/gin-gonic/gin v1.8.1
+	github.com/lib/pq v1.10.7
+)
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.10.0 // indirect
+	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/google/go-cmp v0.5.8 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/ugorji/go/codec v1.2.7 // indirect
+	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
+	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
-	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- k_214api_dev/go/web/main.go
+++ k_214api_dev/go/web/main.go
@@ -1,85 +1,42 @@
 package main
 
 import (
-	"time"
+	"context"
 	"database/sql"
-	"log" // -> ???
-	"context" // -> ???
+	"log"
 
-	"github.com/lib/pq" // -> ???
-	"github.com/gowebexamples/sqlc-example/postgres" // -> ???
+	_ "github.com/lib/pq"
+
 	"github.com/gin-gonic/gin"
-
-	"sqlc_package/build_sqlc"
+	"github.com/myportfolio_k/k_214api_dev/go/web/sqlc/postgres"
 )
-/*
-type Account_master struct {
-	User_id           string    `json:"user_id"`
-	Account_name      string    `json:"account_name"`
-	Mail_addless      string    `json:"mail_addless"`
-	Password          string    `json:"password"`
-	Zip_code          string    `json:"zip_code"`
-	Barth_date        time.Time `json:"barth_date"`
-	Time_zone         string    `json:"time_zone"`
-	Language          string    `json:"language"`
-	Status            string    `json:"status"`
-	Registration_date time.Time `json:"registration_date"`
-}
 
-type Account_attribute struct {
-	Account_id        int       `json:"account_id"`
-	Update_date       time.Time `json:"update_date"`
-	Update_id         string    `json:"update_id"`
-	Pic_id_01         string    `json:"pic_id_01"`
-	Skill             string    `json:"skill"`
-}
-*/
 func main() {
 
-/*---sqlc関連行ここから---------------------------------------------------*/
-	conn, err := sql.Open("build_sqlc", "user=hoge password=passw0rd dbname=sqlc sslmode=desable")
+	/*---sqlc関連行ここから---------------------------------------------------*/
+	db, err := sql.Open("postgres", "user=hoge password=passw0rd dbname=sqlc sslmode=desable")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	db := build_sqlc.New(conn)
-
-	user, err := db.CreateUser(context.Background(), build_sqlc,CreateUserParams{
-	    Firstname: "hoge",
-		Lastname: "fuga",
-	})
-	if err != nil{
+	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println(user)  // 末尾のr.Run()に渡す？
+	//log.Println(user) // 末尾のr.Run()に渡す？
 
-/*---Gin関連行ここから----------------------------------------------------*/
+	/*---Gin関連行ここから----------------------------------------------------*/
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
-/*
-		account_master := new(Account_master)
-  		account_master.User_id = "abcde"
-		account_master.Account_name = "abcde"
-		account_master.Mail_addless = "example@test.go"
-		account_master.Password = "passw0rd"
-		account_master.Zip_code = "123-4567"
-		account_master.Barth_date = time.Now()
-		account_master.Time_zone = "Asia/Tokyo"
-		account_master.Language = "Japanese"
-		account_master.Status = "9"
-		account_master.Registration_date = time.Now()
 
-		account_attribute := new(Account_attribute)
-		account_attribute.Account_id = 1234
-		account_attribute.Update_date = time.Now()
-		account_attribute.Update_id = "update_id"
-		account_attribute.Pic_id_01 = "pic_id_01"
-		account_attribute.Skill = "skill"
-*/
-		// この2行に違和感（ピリオド結合は？）
-		c.JSON(200, account_master)
-		c.JSON(200, account_attribute)
+		queries := postgres.New(db)
+		accountAttribute, err := queries.ListAccount_attribute(context.TODO())
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.JSON(200, accountAttribute)
 	})
 
 	r.Run()
