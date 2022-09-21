182e6210b3a26b113fd338934a0f67edf3ddcc76
Tatsuhide Hirakawa <83848615+tatsuhidehirakawa@users.noreply.github.com>
2022-09-21 13:03:52 +0900

    Update main.go

--- k_214api_dev/go/web/main.go
+++ k_214api_dev/go/web/main.go
@@ -3,19 +3,19 @@ package main
 import (
 	"context"
 	"database/sql"
-//	"fmt"
+	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
-	"sqlc_package"
 	"github.com/gin-gonic/gin"
-//	"github.com/myportfolio_k/k_214api_dev/go/web/sqlc/build_sqlc"
+
+	"sqlc_pkg"
 )
 
 func main() {
 
 	// DB(PostgreSQL)への接続処理(環境変数導入バージョン)
-    cfg := NewConfig()
+	cfg := NewConfig()
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Sslmode) // 「dsn」に結合した文字列を格納
  	db, err := sql.Open("postgres", dsn)
 
