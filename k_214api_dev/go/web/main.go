366031ae06dfe04edbe75eb5f7dcce54008c7e00
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-22 12:07:18 +0900

    Updata comments.

--- k_214api_dev/go/web/main.go
+++ k_214api_dev/go/web/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"context"
 	"database/sql"
-	"fmt"
+//	"fmt"
 	"log"
 
 	_ "github.com/lib/pq"
@@ -13,14 +13,14 @@ import (
 )
 
 func main() {
-
+/*
 	// DB(PostgreSQL)への接続処理(環境変数導入バージョン)
 	cfg := NewConfig()
 	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s sslmode=%s", cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Sslmode) // 「dsn」に結合した文字列を格納
  	db, err := sql.Open("postgres", dsn)
-/*
- 	db, err := sql.Open("postgres", "host=314dbs_dev port=5432 user=postgres password=passw0rd sslmode=disable")
 */
+ 	db, err := sql.Open("postgres", "host=314dbs_dev port=5432 user=postgres password=passw0rd sslmode=disable")
+
 	// DB接続時の例外処理
 	if err != nil {
 		log.Fatal(err)
