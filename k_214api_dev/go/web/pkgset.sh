11749493f55743dbd1989d625dddcc5a7ab5a13a
Tatsuhide Hirakawa <83848615+tatsuhidehirakawa@users.noreply.github.com>
2022-09-22 13:18:10 +0900

    Update pkgset.sh

--- k_214api_dev/go/web/pkgset.sh
+++ k_214api_dev/go/web/pkgset.sh
@@ -4,7 +4,7 @@ touch test1.txt
 touch test2.txt
 
 << COMMENT
-go mod init github.com/goark/pa-api
+go mod init github.com/goark/pa-api                      # この行を何とかする
 go get github.com/kyleconroy/sqlc/cmd/sqlc
 go install github.com/kyleconroy/sqlc/cmd/sqlc
 sqlc version
@@ -12,7 +12,7 @@ sqlc generate --file sqlc/sqlc.yaml
 cd sqlc/build_sqlc
 go mod init sqlc_pkg
 cd ../..
-go get sqlc_pkg
+go get sqlc_pkg                                           # ここまでは動く
 sed -i '$a replace sqlc_pkg => ./sqlc/build_sqlc' go.mod
 go mod tidy
 go run main.go
