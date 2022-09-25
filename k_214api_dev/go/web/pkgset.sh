265c36b1ee9afe4b5c01e9c9b2efa07a1f39a24a
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-25 13:48:55 +0900

    Construct shell script environment.

--- k_214api_dev/go/web/pkgset.sh
+++ k_214api_dev/go/web/pkgset.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/bash
+#!/bin/bash
 
-
-go mod init github.com/tatsuhidehirakawa/myportfolio_k
+go mod init github.com/goark/pa-api
 go get github.com/kyleconroy/sqlc/cmd/sqlc
 go install github.com/kyleconroy/sqlc/cmd/sqlc
-sqlc version
 sqlc generate --file sqlc/sqlc.yaml
 cd sqlc/build_sqlc
 go mod init sqlc_pkg
@@ -12,9 +10,4 @@ cd ../..
 go get sqlc_pkg
 echo 'replace sqlc_pkg => ./sqlc/build_sqlc' >> go.mod
 go mod tidy
-go run main.go
-
-<< COMMENT
-touch test1.txt
-touch test2.txt
-COMMENT
+nohup go run main.go
