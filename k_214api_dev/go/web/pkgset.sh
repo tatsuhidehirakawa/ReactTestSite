a4d061f8a82cbaf0de668974e7a1dac7f3a8db9f
Tatsuhide Hirakawa <83848615+tatsuhidehirakawa@users.noreply.github.com>
2022-09-25 21:16:09 +0900

    Add files via upload
    Manual upload to avoid "line break" issue

---
+++ k_214api_dev/go/web/pkgset.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+go mod init github.com/goark/pa-api
+go get github.com/kyleconroy/sqlc/cmd/sqlc
+go install github.com/kyleconroy/sqlc/cmd/sqlc
+sqlc generate --file sqlc/sqlc.yaml
+cd sqlc/build_sqlc
+go mod init sqlc_pkg
+cd ../..
+go get sqlc_pkg
+echo 'replace sqlc_pkg => ./sqlc/build_sqlc' >> go.mod
+go mod tidy
+nohup go run main.go
