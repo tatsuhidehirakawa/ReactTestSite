7a00f40d1a7e5f6c3b9c0911fce338f87553b6cb
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-09-28 10:07:24 +0900

    Change directory structure.

---
+++ k_214api_dev/src/config.go
@@ -0,0 +1,19 @@
+package main
+
+type Config struct {
+	Host     string
+	User     string
+	Port     string
+	Password string
+	Sslmode  string
+}
+
+func NewConfig() *Config {
+	return &Config{
+		Host:     "314dbs_dev", //本来は各々を環境変数から呼び出すべき
+		Port:     "5432",
+		User:     "postgres",
+		Password: "passw0rd",
+		Sslmode:  "disable",
+	}
+}
---
+++ k_214api_dev/src/sqlc/query.sql
@@ -0,0 +1,39 @@
+-- name: GetAccount_master :one
+SELECT * FROM account_master
+WHERE user_id = ? LIMIT 1;
+
+-- name: ListAccount_master :many
+SELECT * FROM account_master
+ORDER BY user_id;
+
+-- name: CreateAccount_master :execresult
+INSERT INTO account_master (
+-- user_id, account_name, mail_addless, password, zip_code, barth_date, time_zone, language, status, registration_date, rate
+  user_id, account_name, mail_addless, password, zip_code, barth_date, time_zone, language, status, registration_date, rate
+) VALUES (
+  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+);
+
+-- name: DeleteAccount_master :exec
+DELETE FROM account_master
+WHERE user_id = ?;
+
+-- name: GetAccount_attribute :one
+SELECT * FROM account_attribute
+WHERE account_id = ? LIMIT 1;
+
+-- name: ListAccount_attribute :many
+SELECT * FROM account_attribute
+ORDER BY account_id;
+
+-- name: CreateAccount_attribute :execresult
+INSERT INTO account_attribute (
+-- account_id, self_introduction, twitter_url, facebook_url, instagram_url, youtube_channel, update_date, update_id, pic_id_01, pic_id_02, pic_id_03, skill
+  account_id, self_introduction, twitter_url, facebook_url, instagram_url, youtube_channel, update_date, update_id, pic_id_01, pic_id_02, pic_id_03, skill
+) VALUES (
+  ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+);
+
+-- name: DeleteAccount_attribute :exec
+DELETE FROM account_attribute
+WHERE account_id = ?;
