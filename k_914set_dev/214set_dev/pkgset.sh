baa5348e3ba560a10c5aee95ceeafcdb354f6e48
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-03 16:50:43 +0900

    Replaced CRLF with LF for newline characters.

--- k_914set_dev/214set_dev/pkgset.sh
+++ k_914set_dev/214set_dev/pkgset.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-go mod init github.com/goark/pa-api
+# cd /go/src
+touch test.txt
+go mod init github.com/tatsuhidehirakawa/myportfolio_k
 go get github.com/kyleconroy/sqlc/cmd/sqlc
 go install github.com/kyleconroy/sqlc/cmd/sqlc
 sqlc generate --file sqlc/sqlc.yaml
