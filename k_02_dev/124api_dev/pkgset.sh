8cb503c364283c0202e100cdba91194abe70b22f
Tatsuhide Hirakawa <83848615+tatsuhidehirakawa@users.noreply.github.com>
2022-10-04 20:14:49 +0900

    Update pkgset.sh

--- k_02_dev/124api_dev/pkgset.sh
+++ k_02_dev/124api_dev/pkgset.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-go mod init github.com/goark/pa-api
+go mod init github.com/tatsuhidehirakawa/myportfolio_k
 go get github.com/kyleconroy/sqlc/cmd/sqlc
 go install github.com/kyleconroy/sqlc/cmd/sqlc
 sqlc generate --file sqlc/sqlc.yaml
