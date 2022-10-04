6267ab8d17fdafa076482565b1f8dbbe38570f7e
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-04 19:57:00 +0900

    Corrected the location of the instruction description.

--- k_02_dev/124api_dev/pkgset.sh
+++ k_02_dev/124api_dev/pkgset.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# cd /go/src
-touch test.txt
-# go mod init github.com/goark/pa-api
-# go get github.com/kyleconroy/sqlc/cmd/sqlc
-# go install github.com/kyleconroy/sqlc/cmd/sqlc
-# sqlc generate --file sqlc/sqlc.yaml
-# cd sqlc/build_sqlc
-# go mod init sqlc_pkg
-# cd ../..
-# go get sqlc_pkg
-# echo 'replace sqlc_pkg => ./sqlc/build_sqlc' >> go.mod
-# go mod tidy
-# # nohup go run main.go
+go mod init github.com/goark/pa-api
+go get github.com/kyleconroy/sqlc/cmd/sqlc
+go install github.com/kyleconroy/sqlc/cmd/sqlc
+sqlc generate --file sqlc/sqlc.yaml
+cd sqlc/build_sqlc
+go mod init sqlc_pkg
+cd ../..
+go get sqlc_pkg
+echo 'replace sqlc_pkg => ./sqlc/build_sqlc' >> go.mod
+go mod tidy
+nohup go run main.go
