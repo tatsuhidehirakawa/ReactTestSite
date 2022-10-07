9d33e979bb6407550a82646431c27a16c9d380e1
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-08 03:59:15 +0900

    Reconstructed the "create-react-app" command.

---
+++ k_02_dev/110wbs_dev/strset.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#--- <Pattearn 1> Manual Operation.----------------------------#
+
+
+
+
+
+
+#--- <Pattearn 2> create-react-app Operation.------------------#
+rm -rf ./.*
+npm install -g create-react-app
+create-react-app . --typescript
+# # cd 110wbs_dev
+# npm install sass"
