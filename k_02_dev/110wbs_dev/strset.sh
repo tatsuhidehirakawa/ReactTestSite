e473123f2285d413ee194e2ddfc03c0876101fd8
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-08 14:25:23 +0900

    Add remove command.

--- k_02_dev/110wbs_dev/strset.sh
+++ k_02_dev/110wbs_dev/strset.sh
@@ -1,18 +1,32 @@
 #!/bin/bash
 
-#--- <Pattearn 1> Manual Operation.----------------------------#
+# touch test.txt
+
+#--- Remove .gitkeep ------------------------------------------#
 # rm -rf ./.*
+# rm.dotfile
+# rm ./.gitkeep
+# rm -rf tafgetDir/.*
+# rm -rf .gitkeep
+# rm .gitkeep
+# rm -r .*
+# rm -R .*
+# rm -rf
+# rm.dot*
+
+#--- <Pattearn 1> Manual Operation.----------------------------#
 
 # npm install -D webpack webpack-cli
 # npm install -g typescript
 # npm install sass
 
 #--- <Pattearn 2> create-react-app Operation.------------------#
-rm -rf ./.*
-# rm.dotfile
+
 # npm cache clean --force
 # npm install -g create-react-app
-npm install -g create-react-app@latest
-create-react-app . --typescript
-npm install sass@latest
+
+# npm install -g create-react-app@latest
+# create-react-app . --typescript
+# npm install sass@latest
+
 # yarn start
