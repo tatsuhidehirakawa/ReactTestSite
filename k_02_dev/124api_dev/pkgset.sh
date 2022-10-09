43d92cb26b62339a1e695d4f4200e9cbf43aeb6e
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-10 01:41:34 +0900

    Switch shell script operation mode.

--- k_02_dev/124api_dev/pkgset.sh
+++ k_02_dev/124api_dev/pkgset.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# go mod init github.com/tatsuhidehirakawa/myportfolio_k
-# go get github.com/kyleconroy/sqlc/cmd/sqlc
-# go install github.com/kyleconroy/sqlc/cmd/sqlc
-# sqlc generate --file sqlc/sqlc.yaml
-# cd sqlc/build_sqlc
-# # go mod init sqlc_pkg
-# cd ../..
-# go get sqlc_pkg
-# echo 'replace sqlc_pkg => ./sqlc/build_sqlc' >> go.mod
-# go mod tidy
-# nohup go run main.go
+go mod init github.com/tatsuhidehirakawa/myportfolio_k
+go get github.com/kyleconroy/sqlc/cmd/sqlc
+go install github.com/kyleconroy/sqlc/cmd/sqlc
+sqlc generate --file sqlc/sqlc.yaml
+cd sqlc/build_sqlc
+go mod init sqlc_pkg
+cd ../..
+go get sqlc_pkg
+echo 'replace sqlc_pkg => ./sqlc/build_sqlc' >> go.mod
+go mod tidy
+nohup go run main.go
