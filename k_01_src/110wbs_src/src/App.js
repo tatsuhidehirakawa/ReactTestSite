7cc2ef82c6576522f1dd7d5b2a31e8cc749ae689
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-11 13:17:11 +0900

    Fixed codes.

--- k_01_src/110wbs_src/src/App.js
+++ k_01_src/110wbs_src/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Show Sample modal
+          See sample modal
         </a>
       </header>
     </div>
