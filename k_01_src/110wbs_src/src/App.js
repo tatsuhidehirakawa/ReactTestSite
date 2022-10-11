6ce08724df945e7297801d426983d34490ef9a3c
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-12 00:00:34 +0900

    Fixed version No.

--- k_01_src/110wbs_src/src/App.js
+++ k_01_src/110wbs_src/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to the "<code>STGprd_devpkg</code>" v0.3.0
+          Welcome to the "<code>STGprd_devpkg</code>" v0.3.0.0
         </p>
         <a
           className="App-link"
