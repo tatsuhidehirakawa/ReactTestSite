3a1a2f6a0912b2afadd64389c402fe60ee4b7d1e
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-11 14:13:13 +0900

    Fixed comments.

--- k_02_dev/110wbs_dev/strset.sh
+++ k_02_dev/110wbs_dev/strset.sh
@@ -5,7 +5,7 @@
 # npm install -g typescript
 # npm install sass
 
-#--- <Pattearn B> create-react-app Operation.---(6m56sec)------#
+#--- <Pattearn B> create-react-app Operation.---(5m45sec)------#
 # Saveing samplecodes.
 mv public ..
 mv src ..
