2cfeac4037f77410f5549fc0a444b2b56e7dcde3
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-11 13:15:45 +0900

    Fixed codes.

--- k_01_src/110wbs_src/src/App.js
+++ k_01_src/110wbs_src/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome <code>STGprd_devpkg</code> v0.1
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Welcome STGprd_devpkg v0.1
+          Show Sample modal
         </a>
       </header>
     </div>
