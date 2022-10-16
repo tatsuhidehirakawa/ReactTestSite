99fcf0ab625b83a26c0a10021a7e50e2afe652c0
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-16 21:21:36 +0900

    Add put method.

--- k_01_src/124api_src/src/main.go
+++ k_01_src/124api_src/src/main.go
@@ -41,30 +41,30 @@ func main() {
 	}
 
 	/*--- POSTのハンドラ部分 --------------------*/
-	posting := func(c *gin.Context) {
-        // パラメータのstructオブジェクトを作成
-        var attributeParams build_sqlc.CreateAccount_attributeParams
-        // 作成したオブジェクトの参照を渡してJSONデータをstructに注入
-        c.BindJSON(&attributeParams)
-        // DBアクセス用のインスタンスを生成
-        queries := build_sqlc.New(db)
-        // 第二引数で、先程作成したattributeParamsを渡す
-        // ※今回は戻り値のSQLResultは使用しないので、破棄しているが、使用してもOK
-        _, err := queries.CreateAccount_attribute(context.TODO(), attributeParams)
+	osting := func(c *gin.Context) {
+		// パラメータのstructオブジェクトを作成
+		var attributeParams build_sqlc.CreateAccount_attributeParams
+		// 作成したオブジェクトの参照を渡してJSONデータをstructに注入
+		c.BindJSON(&attributeParams)
+		// DBアクセス用のインスタンスを生成
+		queries := build_sqlc.New(db)
+		// 第二引数で、先程作成したattributeParamsを渡す
+		// ※今回は戻り値のSQLResultは使用しないので、破棄しているが、使用してもOK
+		_, err := queries.CreateAccount_attribute(context.TODO(), attributeParams)
 
-        // 以下は任意
-        // POST成功がわかりやすいように、成功時はOKを返却し、失敗時はNGとエラー詳細を返却する
-        if err == nil {
-            c.JSON(http.StatusOK, gin.H{
-                "status": "OK",
-            })
-        } else {
-            c.JSON(http.StatusBadRequest, gin.H{
-                "status": "NG",
-                "detail": err.Error(),
-            })
-        }
-    }
+		// 以下は任意
+		// POST成功がわかりやすいように、成功時はOKを返却し、失敗時はNGとエラー詳細を返却する
+		if err == nil {
+			c.JSON(http.StatusOK, gin.H{
+				"status": "OK",
+			})
+			} else {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"status": "NG",
+				"detail": err.Error(),
+			})
+		}
+	}
 
 	/*--- PUTのハンドラ部分 -----------------------*/
 	putting := func(c *gin.Context) {
@@ -86,19 +86,19 @@ func main() {
 	}
 
 	/*--- DELETEのハンドラ部分 --------------------*/
-    deleting := func(c *gin.Context) {
-        // クエリパラメータからaccountIDを取得
-        accountID := c.Param("accountID")
-        queries := build_sqlc.New(db)
-        err := queries.DeleteAccount_attribute(context.TODO(), accountID)
+	deleting := func(c *gin.Context) {
+		// クエリパラメータからaccountIDを取得
+		accountID := c.Param("accountID")
+		queries := build_sqlc.New(db)
+		err := queries.DeleteAccount_attribute(context.TODO(), accountID)
 
-        if err != nil {
-            log.Fatal(err)
-        }
+		if err != nil {
+			log.Fatal(err)
+		}
 
         c.JSON(200, "Successful deletion")
 
-    }
+	}
 
 	/*--------------------------------------------*/
 
@@ -108,8 +108,8 @@ func main() {
 	router.GET("/someGet", getting)
 	router.POST("/somePost", posting)
 	router.PUT("/somePut", putting)
-    // :accontIDはクエリパラメータ
-    router.DELETE("/someDelete/:accountID", deleting)
+	// :accontIDはクエリパラメータ
+	router.DELETE("/someDelete/:accountID", deleting)
 	// router.PATCH("/somePatch", patching)
 	// router.HEAD("/someHead", head)
 	// router.OPTIONS("/someOptions", options)
--- k_01_src/124api_src/src/sqlc/query.sql
+++ k_01_src/124api_src/src/sqlc/query.sql
@@ -14,6 +14,10 @@ INSERT INTO account_master (
   $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11
 );
 
+-- name: UpdateAccount_master :execresult
+UPDATE account_master SET account_name=$2, mail_addless=$3, password=$4, zip_code=$5, barth_date=$6, time_zone=$7, language=$8, status=$9, registration_date=$10, rate=$11
+WHERE user_id=$1;
+
 -- name: DeleteAccount_master :exec
 DELETE FROM account_master
 WHERE user_id = $1;
@@ -33,6 +37,10 @@ INSERT INTO account_attribute (
 ) VALUES (
   $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
 );
+
+-- name: UpdateAccount_attribute :execresult
+UPDATE account_attribute SET self_introduction=$2, twitter_url=$3, facebook_url=$4,instagram_url=$5, youtube_channel=$6, update_date=$7, update_id=$8, pic_id_01=$9, pic_id_02=$10, pic_id_03=$11, skill=$12
+WHERE account_id=$1; 
 
 -- name: DeleteAccount_attribute :exec
 DELETE FROM account_attribute
