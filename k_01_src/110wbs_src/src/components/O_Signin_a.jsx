758c2383166443a8a9423d2f03561e5f27b266c0
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-19 19:36:26 +0900

    Fixed component dependencies.

--- k_01_src/110wbs_src/src/components/O_Signin_a.jsx
+++ k_01_src/110wbs_src/src/components/O_Signin_a.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 // import A_Button_a from './A_Button_a';
 import './O_Signin_a.scss';
 
-// const MSignina = () => {
+// const O_Signin_a = () => {
 //   const [count, setcount] = React.usestate(0);
 
 class O_Signin_a extends React.Component {
@@ -24,25 +24,26 @@ class O_Signin_a extends React.Component {
     if (this.state.isModalOpen) {
       modal = (
         // <div className="modalBack・ground">   {/* 背景をグレーアウトする処理 */}
-          <div className="modal_inner">
-            <h2 className="notes"><center>Welcome to the "STGprd_devpkg"!</center></h2>
+          <div className="modal_general">
+            <h2 className="upper_notes"><center>Welcome to the "STGprd_devpkg" - Ver 0.3.0.0</center></h2>
             <input
-              className="mailAndpass"
+              className="signIntext"
               // placeholder="Please input your id or email."
-              value="testing-01"
+              value="test_id_01"
             />
             <input
-              className="loginPass"
+              className="signIntext"
               // placeholder="Please input your password."
               value="********"
             />
-            <button className="modal-tx-btn" onClick={() => this.handleClickClose()}>
+            <button className="tx_btn" onClick={() => this.handleClickClose()}>
               Login
             </button>
             {/* <A_Button_a /> */}
-            <div className="notes">
-              If forgetten password, <u>cleck</u>.\nIf you agree to the Terms of Use and Privacy Policy please `<u>login</u>` .`\n
-            `If you do not have an account please `<u>register</u>` .``
+            <div className="lower_notes">
+              If forgetten password, <u>cleck</u>.<br/>
+              If you agree to the Terms of Use and Privacy Policy please <u>login</u> .<br/>
+              If you do not have an account please <u>register</u> .<br/>
             </div>
           </div>
         // </div>
