036304a6ae27d2b203ed690b250e35d1c417d1e1
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-23 22:09:31 +0900

    Fixed codes.

--- k_01_src/110wbs_src/src/components/Lv1_atm/ApiFetch.jsx
+++ k_01_src/110wbs_src/src/components/Lv1_atm/ApiFetch.jsx
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 
 const ApiFetch = () => {
-    console.log('ApiFetch.jsx: ApiFetch()')
+    console.log('ApiFetch.jsx: ApiFetch()')   // Add for Debug.
     const [get, setGet] = useState([])
 
     useEffect(() => {
@@ -9,13 +9,15 @@ const ApiFetch = () => {
         // fetch('https://jsonplaceholder.typicode.com/get', {method: 'GET'})
         fetch('http://localhost:8080/someGet')
 
-        .then(res => {console.log(res);  //
-            res.json()})  //
+        .then(res => {
+            console.log(res);   // Add for Debug.
+            res.json()
+        })
         .then(data => {
             setGet(data)
         })
         .catch(err => {
-            console.log('ApiFetch.jsx: useEffect() err: ', err)
+            console.log('ApiFetch.jsx: useEffect() err: ', err)   // Add for Debug.
         });
     },[])
 
