fdc91a38304c191e5b71286987623ba5fe0fbfec
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-23 20:11:36 +0900

    Add debug codes.

--- k_01_src/110wbs_src/src/components/Lv1_atm/ApiFetch.jsx
+++ k_01_src/110wbs_src/src/components/Lv1_atm/ApiFetch.jsx
@@ -1,16 +1,22 @@
 import React, {useState, useEffect} from 'react'
 
 const ApiFetch = () => {
-
+    console.log('ApiFetch.jsx: ApiFetch()')
     const [get, setGet] = useState([])
 
     useEffect(() => {
+        console.log('ApiFetch.jsx: useEffect()')
         // fetch('https://jsonplaceholder.typicode.com/get', {method: 'GET'})
         fetch('http://localhost:8080/someGet')
-        .then(res => res.json())
+
+        .then(res => {console.log(res);  //
+            res.json()})  //
         .then(data => {
             setGet(data)
         })
+        .catch(err => {
+            console.log('ApiFetch.jsx: useEffect() err: ', err)
+        });
     },[])
 
     return (
