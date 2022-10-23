7bfe606cbec183f4ccd2f5576cd52db716ba3533
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-23 23:29:07 +0900

    Add assets.

---
+++ k_01_src/110wbs_src/src/components/Lv3_orgs/Wrapper_a.jsx
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+    <div className="copy-container">
+        <h1>個人開発練習サイト</h1>
+        <h2>
+          React、Sass、Go、PostgressSQL、Dockerを組み合わせたWeb制作練習サイト
+        </h2>
+        <ul>
+          <li>
+            いろはにほへと ちりぬるを わかよたれそ つねならむ うゐのおくやま
+            けふこえて あさきゆめみし ゑひもせす
+          </li>
+          <li>
+            以呂波耳本へ止 千利奴流乎 和加餘多連曽 津祢那良牟 有為能於久耶万
+            計不己衣天 阿佐伎喩女美之 恵比毛勢須
+          </li>
+          <li>abcdefghijklmnopqrstuvwxyz</li>
+          <li>1234567890</li>
+        </ul>
+    </div>
