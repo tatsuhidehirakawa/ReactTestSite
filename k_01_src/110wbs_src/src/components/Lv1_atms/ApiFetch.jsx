121edafd593fbee7e9de4b6aa10864cd65041646
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-27 22:11:38 +0900

    Fixed version.

--- k_01_src/110wbs_src/src/components/Lv1_atms/ApiFetch.jsx
+++ k_01_src/110wbs_src/src/components/Lv1_atms/ApiFetch.jsx
@@ -9,7 +9,9 @@ const ApiFetch = () => {
         // fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
         // fetch('http://localhost:8080/somePost', {method: 'GET'})
         // fetch('http://host.docker.internal:8080/somePost', {method: 'GET'})
-        fetch('http://host.docker.internal:8080/someGet', {method: 'GET'})
+        // fetch('http://host.docker.internal:8080/someGet', {method: 'GET'})   // Docker for Mac 18.03 and Windows10
+        // fetch('http://docker.for.mac.host.internal:8080/someGet', {method: 'GET'})   // Docker for Mac 17.12
+        fetch('http://docker.for.mac.localhost:8080/someGet', {method: 'GET'})   // Docker for Mac 17.06
         .then(res => res.json())
         .then(data => {
             setPosts(data)
@@ -26,6 +28,7 @@ const ApiFetch = () => {
                     posts.map(post => <li key={post.account_id}>{post.skill}</li>)
                 }
             </ul>
+            
         </div>
     )
 }
