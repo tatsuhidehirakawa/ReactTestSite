73bbb14bc047abfa8f32ca0bc15a888d3cdf5f53
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-28 13:56:32 +0900

    Add files.

--- k_01_src/110wbs_src/src/App.js
+++ k_01_src/110wbs_src/src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Link, Route } from 'react-router-dom';
 import MAIN from './components/Lv4_page/Main.jsx';
-import Page404 from './components/Lv4_page/Page404.tsx';
+import Page404 from './components/Lv4_page/Page404.jsx';
 import ApiFetch from './hooks/ApiFetch.jsx';
 
 function App() {
@@ -27,9 +27,6 @@ function App() {
           <Link to='/ViewPackageSummary' className='App-link'>View Package Summary</Link>
             <Route path="./components/Lv4_page/Main.jsx">
               <MAIN />
-            </Route>
-            <Route path="*S">
-              <Page404 />
             </Route>
         </BrowserRouter>
 
