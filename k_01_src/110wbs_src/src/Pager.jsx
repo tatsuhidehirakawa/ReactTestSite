fddc8f40c4cca87f8cd7ca8a316e292319bfe419
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-10-30 11:54:51 +0900

    Fixed pagesets.

--- k_01_src/110wbs_src/src/Pager.jsx
+++ k_01_src/110wbs_src/src/Pager.jsx
@@ -1,41 +1,41 @@
 import React from 'react'
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 // import App from './App.js'
-import MAIN from './components/tmps/Main.jsx'
+import Main from './components/tmps/Main.jsx'
 import Products from  './components/tmps/Products.jsx'
 import UserGuide from  './components/tmps/UserGuide.jsx'
-import Agreement from  './components/tmps/Agreement.jsx'
-import Pricing from  './components/tmps/Pricing.jsx'
-import Privacy from  './components/tmps/Privacy.jsx'
+// import Agreement from  './components/tmps/Agreement.jsx'
+// import Pricing from  './components/tmps/Pricing.jsx'
+// import Privacy from  './components/tmps/Privacy.jsx'
 import ContactUs from  './components/tmps/ContactUs.jsx'
 import Company from  './components/tmps/Company.jsx'
 import Page404 from  './components/tmps/Page404.jsx'
 
 const Pager = () => {
-    return (
-     <BrowserRouter>
-        <Link to="/" className="App-link">View Package Summary</Link>
-        <Link to="/Products" className="App-link">Products</Link>
-        <Link to="UserGuide" className="App-link">UserGuide</Link>
-        <Link to="/Agreement" className="App-link">Agreement</Link>
-        <Link to="/Pricing" className="App-link">Pricing</Link>
-        <Link to="/Privacy" className="App-link">Privacy</Link>
-        <Link to="/ContactUs" className="App-link">ContactUs</Link>
-        <Link to="/Company" className="App-link">Company</Link>
-        <Link to="/Page404" className="App-link">Page404</Link>
-        <Routes>
-          <Route path={`/`} element={<MAIN />} />
-          <Route path={`/Products/`} element={<Products />} />
-          <Route path={`/UserGuide/`} element={<UserGuide />} />
-          <Route path={`/Agreement/`} element={<Agreement />} />
-          <Route path={`/Pricing/`} element={<Pricing />} />
-          <Route path={`/Privacy/`} element={<Privacy />} />
-          <Route path={`/ContactUs/`} element={<ContactUs />} />
-          <Route path={`/Company/`} element={<Company />} />
-          <Route path={`*`} element={<Page404 />} />
-        </Routes>
-      </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      {/* <Link to="/App" className="App-link">View Package Summary</Link> */}
+      <Link to="/" className="App-link">View Package Summary</Link>
+      <Link to="/Main" className="App-link">Main</Link>
+      <Link to="/Products" className="App-link">Products</Link>
+      <Link to="UserGuide" className="App-link">UserGuide</Link>
+      {/* <Link to="/Agreement" className="App-link">Agreement</Link>
+      <Link to="/Pricing" className="App-link">Pricing</Link>
+      <Link to="/Privacy" className="App-link">Privacy</Link> */}
+      <Link to="/ContactUs" className="App-link">ContactUs</Link>
+      <Link to="/Company" className="App-link">Company</Link>
+      <Routes>
+        <Route path={`/Main`} element={<Main />} />
+        <Route path={`/Products/`} element={<Products />} />
+        <Route path={`/UserGuide/`} element={<UserGuide />} />
+        {/* <Route path={`/Agreement/`} element={<Agreement />} />
+        <Route path={`/Pricing/`} element={<Pricing />} />
+        <Route path={`/Privacy/`} element={<Privacy />} /> */}
+        <Route path={`/ContactUs/`} element={<ContactUs />} />
+        <Route path={`/Company/`} element={<Company />} />
+        <Route path={`*`} element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
 export default Pager;
--- k_01_src/110wbs_src/src/components/tmps/Main.jsx
+++ k_01_src/110wbs_src/src/components/tmps/Main.jsx
@@ -1,27 +1,29 @@
 import React from "react";
 import ApiFetch from '../../hooks/ApiFetch.jsx';
 import O_SIGNIN_A from '../page/O_Signin_a.jsx';
-import Pager from '../../Pager.jsx';
+// import Pager from '../../Pager.jsx';
 import './Main.scss';
 // import Header from "./Header";
 // import Wrapper from "./Wrapper";
 // import Footer from "./Footer";
 
-const MAIN = () => {
+const Main = () => {
 //   render() {
     return (
-      <div className="aaa">
+      <div className="bbb">
+              <p className="Main_text_a">- Main -</p>
+
       {/* <div onClick={()=>{this.handleClickOpen();}}> */}
         {/* <p className="App-link">View Package Summary</p> */}
       {/* </div> */}
         {/* <Header />
         <mWrapper />
         <Footer /> */}
-        <Pager />
-        <ApiFetch />
+        {/* <Pager /> */}
         <O_SIGNIN_A />
+        <ApiFetch />
       </div>
     );
   }
 // }
-export default MAIN;
+export default Main;
