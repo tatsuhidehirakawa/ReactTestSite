dfd3f1f7a393d3f37150234c1f476fc21a1e10b1
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-01 09:10:08 +0900

    Replace database columns.

--- k_01_src/134dbs_src/initdb/03_initdata.sql
+++ k_01_src/134dbs_src/initdb/03_initdata.sql
@@ -2,7 +2,7 @@
 
 INSERT INTO account_attribute
   (account_id,update_date,update_id,pic_id_01,skill)
-VALUES ('5e9nr6eka93u','09/07/22','5e9nr6eka93u','5e9nr6eka93u','SystemAdministrator');
+VALUES ('5e9nr6eka93u','09/07/22','5e9nr6eka93u','5e9nr6eka93u','Successfully loaded the database from PostgreSQL.');
 
 INSERT INTO account_master
   (user_id,account_name,mail_addless,password,zip_code,barth_date,time_zone,language,status,registration_date)
