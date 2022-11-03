07e90105f509786613672f4c4702d5db2ec7b338
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-03 20:22:43 +0900

    Fixed screen layout..

--- k_01_src/110wbs_src/src/components/tmps/Layout.jsx
+++ k_01_src/110wbs_src/src/components/tmps/Layout.jsx
@@ -7,12 +7,11 @@ const Layout = ({children}) => {
   return (
     <>
     <div className="LayoutOutline">
-      {/* <p className="LayoutText">Welcome to the "<code>STGprd_devpkg</code>" - Ver 0.3.0.0</p> */}
-      <ul className="flex-list>">
-        <li className="li1"><Header /></li>
-        <li className="li2">{children}</li>
-        <li className="li3"><Footer /></li>
-      </ul>
+      <div className="flex-list>">
+        <div className="li1"><Header /></div>
+        <div className="li2">{children}</div>
+        <div className="li3"><Footer /></div>
+      </div>
     </div>
     </>
   );
