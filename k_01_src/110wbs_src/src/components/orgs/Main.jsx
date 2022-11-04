49fb5a2545361563e52ee41eb33d8eb06879eab2
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-04 23:32:37 +0900

    Modified screen layout.

--- k_01_src/110wbs_src/src/components/orgs/Main.jsx
+++ k_01_src/110wbs_src/src/components/orgs/Main.jsx
@@ -10,7 +10,15 @@ const Main = () => {
       <p className="userGuide_text_a">
         This Packege is <b>fullstack</b> Web development <b>environment</b> named "<b>STGprd</b>".<br />
         "<b>STGprd</b>" is made by Golang, PostgreSQL, React, TypeScript, Sass & Docker.<br />
-        If you want to use this packege please access "<b>https://github.com/tatsuhidehirakawa/myportfolio_k</b>".
+        If you want to use this packege, please access "<b>https://github.com/tatsuhidehirakawa/myportfolio_k</b>".
+      </p>
+      <p>
+        Technology stack:<br />
+        Frontend: React.js, TypeScript, Sass<br />
+        Backend: Golang(Gin,sqlc), PostgreSQL<br />
+        <b>Dev</b>lopment environment: Docker<br />
+        <b>T</b>e<b>st</b> Environment: CircleCI, Docker(Multistage Build)<br />
+        <b>St</b>a<b>g</b>ing Environment: Terraform
       </p>
       <div className="templete_text"></div>
       <div className="templete_text"></div>
