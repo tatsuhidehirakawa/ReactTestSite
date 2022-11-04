0dd353ebc2091e3b0719641d5c6db64fe755ec6b
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-04 23:53:10 +0900

    Modified screen layout.

--- k_01_src/110wbs_src/src/components/orgs/Main.jsx
+++ k_01_src/110wbs_src/src/components/orgs/Main.jsx
@@ -3,11 +3,12 @@ import "./Main.scss"
 
 const Main = () => {
   return (
-    <div className="bbb">
-      <p className="userGuide_text_a">- Main -</p>
-      <div className="templete_text"></div>
-      <div className="templete_text"></div>
-      <p className="userGuide_text_a">
+    <div className="OutlineA">
+      <div className="InlineA">
+      <p className="TestPageTextA">- Main -</p>
+      <div className="TestPageTextB"></div>
+      <div className="TestPageTextB"></div>
+      <p>
         This Packege is <b>fullstack</b> Web development <b>environment</b> named "<b>STGprd</b>".<br />
         "<b>STGprd</b>" is made by Golang, PostgreSQL, React, TypeScript, Sass & Docker.<br />
         If you want to use this packege, please access "<b>https://github.com/tatsuhidehirakawa/myportfolio_k</b>".
@@ -20,10 +21,12 @@ const Main = () => {
         <b>T</b>e<b>st</b> Environment: CircleCI, Docker(Multistage Build)<br />
         <b>St</b>a<b>g</b>ing Environment: Terraform
       </p>
-      <div className="templete_text"></div>
-      <div className="templete_text"></div>
-      <div className="templete_text"></div>
+      <div className="TestPageTextB"></div>
+      <div className="TestPageTextB"></div>
+      <div className="TestPageTextB"></div>
+      </div>
     </div>
   )
 }
+
 export default Main;
