e7d4bc3ab6ad8f76dd33fef65658aecce51feedd
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-04 22:57:45 +0900

    Modified screen layout.

--- k_01_src/110wbs_src/src/components/orgs/Main.jsx
+++ k_01_src/110wbs_src/src/components/orgs/Main.jsx
@@ -7,12 +7,14 @@ const Main = () => {
       <p className="userGuide_text_a">- Main -</p>
       <div className="templete_text"></div>
       <div className="templete_text"></div>
+      <p className="userGuide_text_a">
+        This Packege is <b>fullstack</b> Web development <b>environment</b> named "<b>STGprd</b>".<br />
+        "<b>STGprd</b>" is made by Golang, PostgreSQL, React, TypeScript, Sass & Docker.<br />
+        If you want to use this packege please access "<b>https://github.com/tatsuhidehirakawa/myportfolio_k</b>".
+      </p>
       <div className="templete_text"></div>
       <div className="templete_text"></div>
       <div className="templete_text"></div>
-      <div className="templete_text"></div>
-      <div className="templete_text"></div>
-      <p className="userGuide_text_a">- Main -</p>
     </div>
   )
 }
