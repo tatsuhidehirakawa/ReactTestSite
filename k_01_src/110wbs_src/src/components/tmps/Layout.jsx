d8b4b2ebd4aaf1f8d0d1b81bb1e7187c263d4b1a
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-06 01:01:40 +0900

    Modified screen layout.

--- k_01_src/110wbs_src/src/components/tmps/Layout.jsx
+++ k_01_src/110wbs_src/src/components/tmps/Layout.jsx
@@ -7,12 +7,12 @@ const Layout = ({children}) => {
   return (
     <>
     <div className="LayoutOutline">
-      <div className="flex-list>">
+      {/* <div className="flex-list>"> */}
         <div className="li1"><Header /></div>
         <div className="li2">{children}</div>
         <div className="li3"><Footer /></div>
       </div>
-    </div>
+    {/* </div> */}
     </>
   );
 }
