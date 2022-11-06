84917ba725fc78efe146e5c2ef5e7b4517789e16
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-06 16:15:34 +0900

    Modified screen layout.

--- k_01_src/110wbs_src/src/components/orgs/Footer.jsx
+++ k_01_src/110wbs_src/src/components/orgs/Footer.jsx
@@ -1,34 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LogoA from '../atms/LogoA.jsx';
 import "./Footer.scss";
 
 const Footer = () => {
   return (
     <>
     <div className="footerOutline">
-      <div className="footer_flex">
-        {/* Logo area */}
-        <div className="footerLogo">
+      <div className="footerInlineA">
+        <div className="footerInlineA_SectionA">
+          <LogoA />
         </div>
-        {/* Left area */}
-        <div className="footerLeft">
+        <div className="footerInlineA_SectionB">
         </div>
-        {/* Left area */}
-        <div className="footerCenter">
+        <div className="footerInlineA_SectionC">
         </div>
-        {/* Right area */}
-        <div className="footerRight">
+        <div className="footerInlineA_SectionD">
           <Link to="/Contact" className="footerText">Contact</Link>
           <Link to="/Recruit" className="footerText">Recruit</Link>
           <Link to="/Company" className="footerText">Company</Link>
           <Link to="/Privacy" className="footerText">Privacy</Link>
         </div>
       </div>
-      {/* Signature area */}
-      {/* <div className="Signature"> */}
-      {/* oupCopyright inc.-3 2022 All Right Reser. All Right Reserved */}
-      <p>2023-2022 &copy; Sample Inc.</p>
-      {/* </div> */}
+      <div className="footerInlineB">
+        {/* oupCopyright inc.-3 2022 All Right Reser. All Right Reserved */}
+        <p>2023-2022 &copy; Sample Inc.</p>
+      </div>
     </div>
     </>
   );
