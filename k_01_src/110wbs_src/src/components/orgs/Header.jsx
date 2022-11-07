85fa86c709d568be0f56fd1d759aabb0b191131c
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-07 17:25:55 +0900

    Modified screen layout.

--- k_01_src/110wbs_src/src/components/orgs/Header.jsx
+++ k_01_src/110wbs_src/src/components/orgs/Header.jsx
@@ -3,31 +3,34 @@ import { Link } from "react-router-dom";
 // import SearchBox from "./SearchBox";
 // import Language from './Language.jsx';
 import SignIn from './Signin.jsx';
+// import SignIn from './Signin.tsx';
 import LogoA from '../atms/LogoA.jsx';
 import "./Header.scss";
 
 const Header = () => {
   return (
-    <div className="header__Outline">
-      <div className="header__SectionA">
+    <div className="header--outline">
+      <div className="header--sectionA__logo">
         <LogoA />
       </div>
-      <div className="header__SectionB--txt">
-        <Link to="/News" className="headerText">News</Link>
-        <Link to="/Products" className="headerText">Products</Link>
+      <div className="header--sectionB__button">
+        <Link to="/News" className="header__text">News</Link>
+        <Link to="/Products" className="header__text">Products</Link>
         {/* <Link to="/TestPage" className="headerText">TestPage</Link> */}
-        <Link to="/Pricing" className="headerText">Pricing</Link>
+        <Link to="/Pricing" className="header__text">Pricing</Link>
       </div>
-      <div className="header__SectionC">
+      <div className="header--sectionC__searchbox">
+        <p>searchbox.jsx</p>
       </div>
-      <div className="header__SectionD--txt">
+      <div className="header--sectionD__button">
         {/* <Language /> */}
-        <Link to="/SiteTop" className="headerText">SiteTop</Link>
-        <Link to="/Agreement" className="headerText">Agreement</Link>
-        <Link to="/UserGuide" className="headerText">UserGuide</Link>
-        <Link to="/SignUp" className="headerText">SignUp</Link>
+        <Link to="/SiteTop" className="header__text">SiteTop</Link>
+        <Link to="/Agreement" className="header__text">Agreement</Link>
+        <Link to="/UserGuide" className="header__text">UserGuide</Link>
+        <Link to="/SignUp" className="header__text">SignUp</Link>
         <SignIn />
-        <Link to="/MyPage" className="headerText">MyPage</Link>
+        {/* <Link to="/SignIn" className="header__text">SignIn</Link> */}
+        <Link to="/MyPage" className="header__text">MyPage</Link>
       </div>
     </div>
   );
