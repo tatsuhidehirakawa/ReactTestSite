ed1b79a0b2136b215406bcf4679016ffea8f7df2
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-24 11:25:55 +0900

    Trying to introduce a tiled display for members.

--- s_01_src/110wbs_src/src/components/orgs/SiteTop.jsx
+++ s_01_src/110wbs_src/src/components/orgs/SiteTop.jsx
@@ -1,53 +1,360 @@
 import React from "react"
+import Card from "../mlcs/Card";
 import "./SiteTop.scss"
-import ApiFetch from '../../hooks/ApiFetch.jsx';
 
 const SiteTop = () => {
+  const customerList = [
+    {
+      name: "壁紙の張り替え得意です！（現状回復）",
+      image: "./sass.svg",
+      introduction: "息子がお世話になっております。草刈り界の重鎮です。"
+    },
+    {
+      name: "どんな鍵にも対応できます！（鍵開け）",
+      image: "../../../public/Assets/react2.svg",
+      introduction:
+        "３以内に解錠できなかったら返金いたします。"
+    },
+    {
+      name: "マイ草刈り機持ってます！（草刈り）",
+      image: "./golang.svg",
+      introduction: "雑草に恐れられています。"
+    },
+    {
+      name: "ゴミ屋敷は私にお任せ下さい！（ゴミ回収）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "ご依頼いただければ出動させます。"
+    },
+    {
+      name: "あなたのお庭、奇麗にします！",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "前職はキャディです。"
+    },
+    {
+      name: "指先から葉緑素を吸収します",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "のび太です。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "清掃はお任せください！（敷地内清掃）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "彼らが通った後にはぺんぺん草一つ生えません。"
+    },
+    {
+      name: "ヤギ２匹派遣します！（草刈り）",
+      image: "../../../public/Assets/postgresql.svg",
+      introduction:
+        "二馬力ならぬ二山羊力です。彼らが通った後にはぺんぺん草一つ生えません"
+    }
+  ];
+
   return (
-    <div className="sitetop--outline">
-
-      <div className="sitetop--inlineA">
-      {/* <p className="sitetop__textA">- <b>SiteTop</b> -</p> */}
-      <div className="sitetop--inlineA--boxA__pictureA">
-      </div>
-      <div className="sitetop--inlineA--boxB__textA">
-        <p className="sss">
-          It is a full-stack package for web development, 
-          focusing on the technology stack presented in the next block. 
-          It is optimized primarily as a template for creating large-scale CtoC sites. 
-          In particular, it is RESTful,
-          does not use CSS-in-JS, minimizes Paas dependencies, 
-          emphasizes orthogonality and loose coupling, static typing, 
-          and Docker usage.
-        </p>
-      </div>
-      </div>
-
-      <div className="sitetop--inlineB">
-        <div className="sitetop--inlineB--boxA__pictureA">
-          <p>
-            Technology stack:<br />
-            Frontend: React.js, TypeScript, Sass<br />
-            Backend: Golang(Gin,sqlc), PostgreSQL<br />
-            <b>Dev</b>lopment environment: Docker<br />
-            <b>T</b>e<b>st</b> Environment: CircleCI, Docker(Multistage Build)<br />
-            <b>St</b>a<b>g</b>ing Environment: Terraform
-          </p>
+    <div className="main-wrapper">
+      <div className="main">
+        <div className="lesson-container">
+          {customerList.map((lessonItem) => {
+            return (
+              <Card
+                name={lessonItem.name}
+                image={lessonItem.image}
+                introduction={lessonItem.introduction}
+              />
+            );
+          })}
         </div>
-        <div className="sitetop--inlineB--boxB__textA">
-          <div className="sitetop__textS"></div>
-          <div className="sitetop__textS"></div>
-          <div className="sitetop__textS"></div>
-          <ApiFetch />
-          <div className="sitetop__textS"></div>
-          <div className="sitetop__textS"></div>
-          <div className="sitetop__textS"></div>
-          <div className="sitetop__textS"></div>
-        </div>
-
       </div>
-      <div className="sitetop--inlineC__pictureA">
-        </div>
     </div>
   );
 }
