158ffdcbc04cc094f9fcd4304a0774e64fb1f476
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-25 21:58:00 +0900

    Database records were tiled on React.

--- s_01_src/110wbs_src/src/components/orgs/SiteTop.jsx
+++ s_01_src/110wbs_src/src/components/orgs/SiteTop.jsx
@@ -1,44 +1,48 @@
-import React from "react"
+// import React from "react"
 import Card from "../mlcs/Card";
 import "./SiteTop.scss"
 import ApiFetch from "../../hooks/ApiFetch";
+import React, { useState, useEffect } from 'react';
 
 const SiteTop = () => {
+  const [data, setData] = useState({ hits: [] });
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    // console.log('ApiFetch.jsx: useEffect()')   // Debug.
+    // fetch('https://jsonplaceholder.typicode.com/posts', {method: 'GET'})
+    // fetch('http://localhost:8080/somePost', {method: 'GET'})
+    // fetch('http://host.docker.internal:8080/somePost', {method: 'GET'})
+    // fetch('http://host.docker.internal:8080/someGet', {method: 'GET'})   // Docker for Mac 18.03 and Windows10
+    // fetch('http://docker.for.mac.host.internal:8080/someGet', {method: 'GET'})   // Docker for Mac 17.12
+    fetch('http://docker.for.mac.localhost:8080/someGet', {method: 'GET'})   // Docker for Mac 17.06
+    .then(res => res.json())
+    .then(data => {
+      setPosts(data)
+      console.log(data)
+      })
+      // .catch(err => {   // Debug.
+      //     console.log('ApiFetch.jsx: useEffect(): err: ', err)
+      // });
+  },[])
+
   const customerList = [
     {
-      name: "どんな鍵にも対応できます！（鍵開け）",
-      image: "../../../public/Assets/react2.svg",
-      introduction:
-        "３以内に解錠できなかったら返金いたします。"
+      name: <ApiFetch>data.account_id</ApiFetch>,
+      image: <ApiFetch>data.pic_id</ApiFetch>,
+      introduction: data.skill
     },
-    {
-      name: <ApiFetch />,
-      image: "./golang.svg",
-      introduction: "雑草に恐れられています。"
-    },
-    {
-      name: "ゴミ屋敷は私にお任せ下さい！（ゴミ回収）",
-      image: "../../../public/Assets/postgresql.svg",
-      introduction:
-        "ご依頼いただければ出動させます。"
-    },
-    {
-      name: "ヤギ２匹派遣します！（草刈り）",
-      image: "../../../public/Assets/postgresql.svg",
-      introduction:
-        "二馬力ならぬ二山羊力です。彼らが通った後にはぺんぺん草一つ生えません"
-    }
   ];
+ 
   return (
     <div className="main-wrapper">
       <div className="main">
         <div className="sitetop--inline">
-          {customerList.map((productItem) => {
+          {posts.map((post) => {
             return (
               <Card
-                name={productItem.name}
-                image={productItem.image}
-                introduction={productItem.introduction}
+                name={post.skill}
+                // image={productItem.image}
+                // introduction={productItem.introduction}
               />
             );
           })}
