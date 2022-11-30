2c32a2f34769a487688e2728f5b8f188175eea09
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-11-30 09:07:25 +0900

    Add tables.

--- s_01_src/124api_src/src/sqlc/schema.sql
+++ s_01_src/124api_src/src/sqlc/schema.sql
@@ -1,17 +1,3 @@
-CREATE TABLE account_master (
-  user_id SERIAL PRIMARY KEY,
-  account_name      text      NOT NULL,
-  mail_addless      text      NOT NULL,
-  password          text      NOT NULL,
-  zip_code          text      NOT NULL,
-  barth_date        date      NOT NULL,
-  time_zone         text      NOT NULL,
-  language          text      NOT NULL,
-  status            text      NOT NULL,
-  registration_date timestamp NOT NULL,
-  rate              text
-);
-
 CREATE TABLE account_attribute (
   account_id        text    NOT NULL,
   self_introduction text,
@@ -25,6 +11,20 @@ CREATE TABLE account_attribute (
   pic_id_02         text,
   pic_id_03         text,
   skill             text      NOT NULL
+);
+
+CREATE TABLE account_master (
+  user_id SERIAL PRIMARY KEY,
+  account_name      text      NOT NULL,
+  mail_addless      text      NOT NULL,
+  password          text      NOT NULL,
+  zip_code          text      NOT NULL,
+  barth_date        date      NOT NULL,
+  time_zone         text      NOT NULL,
+  language          text      NOT NULL,
+  status            text      NOT NULL,
+  registration_date timestamp NOT NULL,
+  rate              text
 );
 
 CREATE TABLE offer_master (
