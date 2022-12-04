1e28966eaa9923777c6a8f2ed52ed26339fcaa1e
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-04 19:31:57 +0900

    Fixed bug where Tailwind CSS was not applied.

--- s_01_src/110wbs_src/src/components/atms/Card.jsx
+++ s_01_src/110wbs_src/src/components/atms/Card.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "./Card.scss";
 
 class Card extends React.Component {
   render() {
 
     return (
       <div className="inline-block w-64 h-72 text-center rounded-2xl text-base leading-normal	m-2">
-        <div className="card--inline rounded">
+        <div className="rounded">
           <Link to="/Offer">
             <img src={this.props.image} alt={this.props.introduction} className="w-60 h-60 rounded-2xl cursor-pointer" />
           </Link>
