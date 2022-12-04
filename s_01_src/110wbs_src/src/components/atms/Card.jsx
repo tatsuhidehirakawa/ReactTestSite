c98b9866db595eccf3c542238558283c414dcd38
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-04 19:29:11 +0900

    Fixed bug where Tailwind CSS was not applied.

--- s_01_src/110wbs_src/src/components/atms/Card.jsx
+++ s_01_src/110wbs_src/src/components/atms/Card.jsx
@@ -1,56 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Card.scss";
-// import { IconButton } from "@material-ui/core";
-// import FavoriteBorder from "@material-ui/icons/FavoriteBorder";
+// import "./Card.scss";
 
 class Card extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { isModalOpen: false }; // 初期のstateにfalseをセット
-  // }
-
-  // handleClickLesson() {
-  //   this.setState({ isModalOpen: true }); // 開く場合のstateにtrueをセット
-  // }
-
-  // handleClickClose() {
-  //   this.setState({ isModalOpen: false }); // 閉じる場合のstateにfalseをセット
-  // }
-
   render() {
-    // let modal;
-    // if (this.state.isModalOpen) {
-    //   modal = (
-    //     <div className="modalBackground">
-    //       <div className="modal-inner">
-    //         <div className="modal-header"></div>
-    //         <div className="modal-introduction">
-    //           <h2>{this.props.name}</h2>
-    //           <p>{this.props.introduction}</p>
-    //         </div>
-    //         <button className="modal-close-btn" onClick={()=>this.handleClickClose()}>
-    //           とじる
-    //         </button>
-    //       </div>
-    //     </div>
-    //   );
-    // }
 
     return (
-      <div className="card--outline">
-        {/* <div className="card--inline" onClick={() => {this.handleClickLesson();}}> */}
-        <div className="card--inline">
+      <div className="inline-block w-64 h-72 text-center rounded-2xl text-base leading-normal	m-2">
+        <div className="card--inline rounded">
           <Link to="/Offer">
-            <img src={this.props.image} alt={this.props.introduction}/>
+            <img src={this.props.image} alt={this.props.introduction} className="w-60 h-60 rounded-2xl cursor-pointer" />
           </Link>
-          {/* <IconButton color="primary" onClick={() => this.props.snsPost()}>
-            <FavoriteBorder />
-          </IconButton> */}
         </div>
         <p>{this.props.name}</p>
         <p><b>{this.props.location}</b>、日本、{this.props.distance}km</p>
-        {/* {modal} */}
       </div>
     );
   }
