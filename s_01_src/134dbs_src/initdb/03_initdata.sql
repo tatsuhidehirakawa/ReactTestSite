0f697efc44fd1700b6eee756c5be50eebdb746de
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-06 03:08:27 +0900

    Add initial data.

--- s_01_src/134dbs_src/initdb/03_initdata.sql
+++ s_01_src/134dbs_src/initdb/03_initdata.sql
@@ -2,16 +2,16 @@
 
 INSERT INTO purchase_histroy
   (purchase_id,purchase_date,purchase_time,payer_account_id,payee_account_id,amount,create_date,update_date)
-VALUES ('5e9nr6eka93u','03/01/19','2019-03-01 10:00:00','5e9nr6eka93u','5e9nr6eka93u',5000,'11/30/22','09/07/22');
+VALUES ('5e9nr6eka93u','2019/03/01','10:00:00','5e9nr6eka93u','5e9nr6eka93u',5000,'2022/11/30 0:00:00','2022/09/07 0:00:00');
 
 INSERT INTO account_attribute
-  (account_id,update_date,update_id,pic_id_01,skill)
-VALUES ('5e9nr6eka93u','09/07/22','5e9nr6eka93u','5e9nr6eka93u','Successfully loaded the database from PostgreSQL.');
+  (account_id,skill,create_date,update_date)
+VALUES ('5e9nr6eka93u','abc','2022/11/30 0:00:00','2022/11/30 0:00:00');
 
 INSERT INTO account_master
-  (account_id,account_name,mail_addless,password,zip_code,barth_date,time_zone,language,status,registration_date)
-VALUES ('5e9nr6eka93u','Admin','Admin@example.com','a4cjuzn2','123-4567','09/07/22','Asia/Tokyo','Japanese',9,'09/07/22');
+  (account_id,account_name,mail_addless,password,zip_code,barth_date,time_zone,language,registration_date,create_date,update_date)
+VALUES ('5e9nr6eka93u','Admin','Admin@example.com','a4cjuzn2','123-4567','09/07/22','Asia/Tokyo','Japanese','09/07/22','2022/11/30 0:00:00','2022/09/07 0:00:00');
 
 INSERT INTO offer_list
   (offer_id,account_id,offer_type,offer_title,offer_status,offer_text,create_date,update_date)
-VALUES ('5e9nr6eka93u','5e9nr6eka93u','1','xyz','2','abc','09/07/22','09/07/22');
+VALUES ('5e9nr6eka93u','5e9nr6eka93u','1','xyz','2','abc','2009/07/22 0:00:00','2009/07/22 0:00:00');
