f2342493a8153fac464d39d26bf192b660da9cd0
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-06 09:23:48 +0900

    Add tables.

--- s_01_src/124api_src/src/sqlc/schema.sql
+++ s_01_src/124api_src/src/sqlc/schema.sql
@@ -1,44 +1,52 @@
-CREATE TABLE account_attribute (
-  account_id        text    NOT NULL,
-  self_introduction text,
-  twitter_url       text,
-  facebook_url      text,
-  instagram_url     text,
-  youtube_channel   text,
-  update_date       timestamp NOT NULL,
-  update_id         text      NOT NULL,
-  pic_id_01         text      NOT NULL,
-  pic_id_02         text,
-  pic_id_03         text,
-  skill             text      NOT NULL
-);
-
 CREATE TABLE account_master (
-  user_id           text      NOT NULL,
-  account_name      text      NOT NULL,
-  mail_addless      text      NOT NULL,
-  password          text      NOT NULL,
-  zip_code          text      NOT NULL,
-  barth_date        date      NOT NULL,
-  time_zone         text      NOT NULL,
-  language          text      NOT NULL,
-  status            text      NOT NULL,
-  registration_date timestamp NOT NULL,
-  rate              text
+  account_id          text      NOT NULL,
+  account_name        text      NOT NULL,
+  mail_addless        text      NOT NULL,
+  password            text      NOT NULL,
+  zip_code            text      NOT NULL,
+  barth_date          date      NOT NULL,
+  time_zone           text      NOT NULL,
+  language            text      NOT NULL,
+  registration_date   timestamp NOT NULL,
+  create_date         timestamp NOT NULL,
+  update_date         timestamp NOT NULL
 );
 
-CREATE TABLE offer_master (
-  offer_id          text    NOT NULL,
-  user_id           text    NOT NULL,
-  offer_type        text    NOT NULL,
-  offer_title       text    NOT NULL,
-  offer_severity    text,
-  offer_catagory    text,
-  offer_locktion    text,
-  offer_pic_1       text    NOT NULL,
-  offer_pic_2       text,
-  offer_pic_3       text,
-  offer_pic_4       text,
-  offer_pic_5       text,
-  offer_text        text    NOT NULL
+CREATE TABLE account_attribute (
+  account_id          text      NOT NULL,
+  self_introduction   text,
+  twitter_uri         text,
+  facebook_uri        text,
+  instagram_uri       text,
+  youtube_channel_uri text,
+  skill               text      NOT NULL,
+  rate                text,
+  status              text,
+  create_date         timestamp NOT NULL,
+  update_date         timestamp NOT NULL
+);
+
+CREATE TABLE offer_List (
+  offer_id            text      NOT NULL,
+  account_id          text      NOT NULL,
+  offer_type          text      NOT NULL,
+  offer_title         text      NOT NULL,
+  offer_severity      text,
+  offer_catagory      text,
+  offer_location      text,
+  offer_status        text      NOT NULL,
+  offer_text          text      NOT NULL,
+  create_date         timestamp NOT NULL,
+  update_date         timestamp NOT NULL
+);
+
+CREATE TABLE purchase_histroy (
+  purchase_id         text      NOT NULL,
+  purchase_date       text      NOT NULL,
+  purchase_time       text      NOT NULL,
+  payer_account_id    text      NOT NULL,
+  payee_account_id    text      NOT NULL,
+  amount              text      NOT NULL,
+  create_date         timestamp NOT NULL,
+  update_date         timestamp NOT NULL
 );
