fb2dadfe3b1628cdd86a45e183a64c268ca1a03e
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-09 23:52:30 +0900

    Try to fix z-index.

--- s_01_src/110wbs_src/src/components/atms/Card.jsx
+++ s_01_src/110wbs_src/src/components/atms/Card.jsx
@@ -5,10 +5,10 @@ class Card extends React.Component {
   render() {
 
     return (
-      <div className="bg-white inline-block w-64 h-72 shadow-lg p-[10px 10px 20px 10px] text-center rounded-2xl text-base leading-normal m-2 z-6">
+      <div className="bg-white inline-block w-[192px] h-[54px] shadow-lg p-[10px 10px 20px 10px] text-center rounded-2xl text-base leading-normal m-2 z-6">
         <div className="rounded">
           <Link to="/Offer">
-            <img src={this.props.image} alt={this.props.introduction} className="w-full h-60 rounded-2xl cursor-pointer p-[15px 5px 0px 15px]" />
+            <img src={this.props.image} alt={this.props.introduction} className="w-full h-[180px] rounded-2xl cursor-pointer p-[15px 5px 0px 15px]" />
           </Link>
         </div>
         <p>{this.props.name}</p>
