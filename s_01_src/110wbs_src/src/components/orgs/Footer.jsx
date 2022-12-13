d20312feea4af6af4f0b3dd84e415091a3380db4
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-13 17:55:24 +0900

    Fixed codes.

--- s_01_src/110wbs_src/src/components/orgs/Footer.jsx
+++ s_01_src/110wbs_src/src/components/orgs/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
     position: absolute;
     bottom: 0;
     z-index: 9;
-    // @include frostedglass;
+    @include frostedglass;
   `;
 
   const inlineA = css`
@@ -31,57 +31,70 @@ const Footer = () => {
     height: 35px;
     p {
       margin-top: 30px;
-      @include footer__Text;
     }
   `;
 
-  const inlneA = css`
-    display: flex;
-    height: 173px;
-    justify-content: space-between;
+  const LogoBox = css`
+    margin: 43px 0px 0px 90px;
+    float: left;
+    height: 100px;
+    width: 10%;
+    .logoInline {
+      height: 90px;
+    }
   `;
 
-  const footer__Text = css`
+  const SectionBox = css`
+    width: 9%;
+    height: 10px;
+    display: flex;
+    flex-direction: column;
+    line-height: 1.4;
+    text-align: center;
+    margin: 15px 21px 5px 0px;
+  `;
+
+  const footerText = css`
     color: rgb(26, 24, 24);
     text-decoration: none;
     text-shadow: 1px 2px 3px #808080;
-    cursor: pointer;
+    font-weight: bold;  // もっと太くしたい
   `;
 
   return (
     <>
     <div className="footer--Outline" css={outline}>
       <div className="footer--inlineA" css={inlineA}>
-        <div className="footer--inlineA--sectionA_logo" css={sectionA}>
+        <div className="footer--inlineA--sectionA_logo" css={LogoBox}>
           <LogoA />
         </div>
-        <div className="footer--inlineA--sectionB__blank" css={sectionB}>
+        <div className="footer--inlineA--sectionB__blank" css={SectionBox}>
           <p>Company</p>
-          <Link to="/Team" className="footer__Text" css={footer__Text}>Team</Link>
-          <Link to="/none" className="footer__Text" css={footer__Text}>Histroy</Link>
-          <Link to="/Contact" className="footer__Text" css={footer__Text}>Contact</Link>
-          <Link to="/none" className="footer__Text" css={footer__Text}>Locations</Link>
+          <Link to="/Team" css={footerText}>Team</Link>
+          <Link to="/none" css={footerText}>Histroy</Link>
+          <Link to="/Contact" css={footerText}>Contact</Link>
+          <Link to="/none" css={footerText}>Locations</Link>
         </div>
-        <div className="footer--inlineA--sectionC__blank" css={sectionC}>
+        <div className="footer--inlineA--sectionC__blank" css={SectionBox}>
           <p>Features</p>
-          <Link to="/none" className="footer__Text" css={footer__Text}>Cool stuff</Link>
-          <Link to="/none" className="footer__Text" css={footer__Text}>Random feature</Link>
-          <Link to="/none" className="footer__Text" css={footer__Text}>Team feature</Link>
-          <Link to="/none" className="footer__Text" css={footer__Text}>Developer staff</Link>
-          <Link to="/Recruit" className="footer__Text" css={footer__Text}>Recruit</Link>
+          <Link to="/none" css={footerText}>Cool stuff</Link>
+          <Link to="/none" css={footerText}>Random feature</Link>
+          <Link to="/none" css={footerText}>Team feature</Link>
+          <Link to="/none" css={footerText}>Developer staff</Link>
+          <Link to="/Recruit" css={footerText}>Recruit</Link>
         </div>
-        <div className="footer--inlineA--sectionD__text" css={sectionD}>
+        <div className="footer--inlineA--sectionD__text" css={SectionBox}>
           <p>Resources</p>
-          <Link to="/ResourceA" className="footer__Text" css={footer__Text}>ResourceA</Link>
-          <Link to="/ResourceB" className="footer__Text" css={footer__Text}>ResourceB</Link>
-          <Link to="/Company" className="footer__Text" css={footer__Text}>Company</Link>
-          <Link to="/ResourceC" className="footer__Text" css={footer__Text}>ResourceC</Link>
+          <Link to="/ResourceA" css={footerText}>ResourceA</Link>
+          <Link to="/ResourceB" css={footerText}>ResourceB</Link>
+          <Link to="/Company" css={footerText}>Company</Link>
+          <Link to="/ResourceC" css={footerText}>ResourceC</Link>
         </div>
-        <div className="footer--inlineA--sectionE__text" css={sectionE}>
+        <div className="footer--inlineA--sectionE__text" css={SectionBox}>
           <p>Legal</p>
-          <Link to="/Contact" className="footer__Text" css={footer__Text}>Contact</Link>
-          <Link to="/Privacy" className="footer__Text" css={footer__Text}>Privacy</Link>
-          <Link to="/none" className="footer__Text" css={footer__Text}>Temes fo use</Link>
+          <Link to="/Contact" css={footerText}>Contact</Link>
+          <Link to="/Privacy" css={footerText}>Privacy</Link>
+          <Link to="/none" css={footerText}>Temes fo use</Link>
         </div>
       </div>
       <div className="footer--InlineB__signeture" css={inlineB}>
