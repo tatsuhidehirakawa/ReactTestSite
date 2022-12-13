671987740bd032a50310c43e69e1e2dcdb6450b8
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-13 18:52:23 +0900

    Add EmotionCSS.

--- s_01_src/110wbs_src/src/components/orgs/Header.jsx
+++ s_01_src/110wbs_src/src/components/orgs/Header.jsx
@@ -4,36 +4,86 @@ import { Link } from "react-router-dom";
 import LogoA from '../atms/LogoA.jsx';
 import Tags from '../mlcs/SearchBox.tsx';
 // import SignIn from './SignIn.jsx';
-import "./Header.scss";
-
+import "../../Pager.scss";
 
 const Header = () => {
+
+  const outline = css`
+    display: flex;
+    justify-content: space-between;
+    position: fixed;
+    top: 0;
+    height: 69px;
+    z-index: 10;
+    width: 100%;
+    background-color: #ffffff;
+    box-shadow: 0 10px 25px 0 rgba(0, 0, 0, .1);
+  `;
+
+  const sectionA = css`
+    display: flex;
+    margin: 0px 0px 0px 15px;
+    height: 69px;
+    width: 30%;
+  `;
+
+  const logo = css`
+    margin: 7px 0px 0px 15px;
+    height: 69px;
+    width: 19%;
+  `;
+
+  const boxB = css`
+    margin: 25px 0px 0px 0px;
+    width: 22%;
+  `;
+
+  const text = css`
+    margin: 0px 3px 3px 30px;
+    color: rgb(26, 24, 24);
+    padding: 0px 0px 10px 0px;
+    text-decoration: none;
+    text-shadow: 1px 2px 3px #808080;
+    cursor: pointer;
+  `;
+
+  const searchbox = css`
+    margin: 7px 33px 6px 33px
+  `;
+
+  const sectionC = css`
+    display: flex;
+    padding: 0px;
+    margin: 25px 110px 0px 0px;
+    width: 30%;
+  `;
+
   return (
-    <div className="header--outline">
-      <div className="header--sectionA">
-        <div className="header--sectionA--boxA__logo">
+    <div className="header--outline" css={outline}>
+      <div className="header--sectionA" css={sectionA}>
+        <div className="header--sectionA--boxA__logo" css={logo}>
           <LogoA />
         </div>
-        <div className="header--sectionA--boxB__button">
-          <Link to="/News" className="header__text">News</Link>
-          <Link to="/Products" className="header__text">Products</Link>
-          <Link to="/Pricing" className="header__text">Pricing</Link>
+        <div className="header--sectionA--boxB__button" css={boxB}>
+          <Link to="/News" className="header__text" css={text}>News</Link>
+          <Link to="/Products" className="header__text" css={text}>Products</Link>
+          <Link to="/Pricing" className="header__text" css={text}>Pricing</Link>
         </div>
       </div>
-      <div className="header--sectionB__searchbox">
+      <div className="header--sectionB__searchbox" css={searchbox}>
         <Tags />
       </div>
-      <div className="header--sectionC__button">
+      <div className="header--sectionC__button" css={sectionC}>
         {/* <Language /> */}
-        <Link to="/SiteTop" className="header__text">SiteTop</Link>
-        <Link to="/Agreement" className="header__text">Agreement</Link>
+        <Link to="/SiteTop" className="header__text" css={text}>SiteTop</Link>
+        <Link to="/Agreement" className="header__text" css={text}>Agreement</Link>
         {/* <Link to="/Agreement" className="text-9xl">Agreement</Link> */}
-        <Link to="/UserGuide" className="header__text">UserGuide</Link>
-        <Link to="/SignUp" className="header__text">SignUp</Link>
+        <Link to="/UserGuide" className="header__text" css={text}>UserGuide</Link>
+        <Link to="/SignUp" className="header__text" css={text}>SignUp</Link>
         {/* <SignIn className="header__text">kkk</SignIn> */}
         {/* <SignIn className="header__text" /> */}
-        <Link to="/SignIn" className="header__text">SignIn</Link>
-        <Link to="/MyPage" className="header__text">MyPage</Link>
+        <Link to="/SignIn" className="header__text" css={text}>SignIn</Link>
+        <Link to="/MyPage" className="header__text" css={text}>MyPage</Link>
       </div>
     </div>
   );
