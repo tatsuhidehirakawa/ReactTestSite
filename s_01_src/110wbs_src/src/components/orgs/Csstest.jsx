b7a88482fe9015c57a6f0da33f45aaaaca1c1005
Tatsuhide Hirakawa <tatsuhide.hirakawa+s@hirasyo.com>
2022-12-13 16:43:13 +0900

    Modify csstest.

--- s_01_src/110wbs_src/src/components/orgs/Csstest.jsx
+++ s_01_src/110wbs_src/src/components/orgs/Csstest.jsx
@@ -1,7 +1,6 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import React from "react"
-import './Csstest.scss';
 
 const Csstest = () => {
 
@@ -14,9 +13,8 @@ const Csstest = () => {
   return (
     <>
       <div className="card--outline" >
-        <p style={{fontSize:"20px",marginBottom:"50px"}}>CSS-in-JS test components</p>
+        <p style={{fontSize:"20px",marginBottom:"28px"}}>CSS-in-JS test components</p>
         <p style={{color:"red",fontSize:"20px"}}>This is inline style(orange)</p>
-        <p className="csstest_sass">This is Sass(blue)</p>
         <p css={csstest_emotion}>This is Emotion(red)</p>
         <p className="text-green-600 text-xl text-center">This is TailwindCSS(green)</p>
       </div>
